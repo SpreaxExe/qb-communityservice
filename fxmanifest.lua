5da177d4e69af77023e187037371fad36424a313
SpreaxExe <spreax.exe@gmail.com>
2023-02-16 14:45:56 +0000

    update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,17 @@ game 'gta5'
 
 server_scripts {
 	'config.lua',
-	'server/main.lua'
+	'server/main.lua',
 }
 
 client_scripts {
 	'config.lua',
-	'client/main.lua'
+	'client/main.lua',
 }
+
+escrow_ignore {
+	'config/config.lua',
+	'server/version_check',
+}
+
+lua54 'yes'
